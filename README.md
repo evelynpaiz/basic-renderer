9b48d103be3c5289e3fd92aea95a49f5717931fc
epaizreyes <evelyn.rpaiz@gmail.com>
2023-02-23 14:19:00 +0100

    Create a window

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Basic Renderer
 This is a lightweight rendering pipeline using OpenGL.
 
+## Requirements
+
+* [OpenGL](https://www.opengl.org/): used as a low level rendering library.
+* [3rd Party Libraries]: external libraries needed in this project.
+    * [glfw](https://github.com/glfw/glfw): Creation of windows, contexts and surfaces. It also receives inputs and events.
+
 ## References
 The three main references for this rendering engine are:
-* [Hazel Engine](https://github.com/TheCherno/Hazel)
 * [Learn OpenGL](https://github.com/JoeyDeVries/LearnOpenGL)
 * [OpenGL Tutorial](https://github.com/VictorGordan/opengl-tutorials)
+* [Hazel Engine](https://github.com/TheCherno/Hazel)
--- renderer/src/main.cpp
+++ renderer/src/main.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
 
+#ifdef __APPLE__
+    #define GL_SILENCE_DEPRECATION
+#endif
+
+#include <GLFW/glfw3.h>
+
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-    std::cin.get();
+    // Initialize GLFW
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW!" << std::endl;
+        return -1;
+    }
+    
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow* window;
+    window = glfwCreateWindow(800, 600, "OpenGL Tutorial", nullptr, nullptr);
+    
+    if (!window)
+    {
+        std::cout << "Failed to create a GLFW window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+    
+    // Loop until the user closes the window
+    while (!glfwWindowShouldClose(window))
+    {
+        // Render
+        glClearColor(0.93f, 0.93f, 0.93f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.0f,  0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glEnd();
+        
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+        
+        // Poll for and process events
+        glfwPollEvents();
+    }
+    
+    // Terminate (GLFW)
+    glfwTerminate();
 }
